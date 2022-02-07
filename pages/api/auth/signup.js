6734c97e6dee1f8d7c1cf254a948adee32dfc0f5
amir hoseein <amiirhossein.mpr@gmail.com>
2022-02-08 00:49:21 +0330

    Sending a _Signin_ Request From The Frontend

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,6 +1,9 @@
 import { connectedToDatabase } from '../../../lib/db';
 import { hashPassword } from '../../../lib/auth';
 function Handler(req, res) {
+  if (req.method !== 'POST') {
+    return;
+  }
   const data = req.body;
   const { email, password } = data;
 
@@ -16,7 +19,7 @@ function Handler(req, res) {
 
   const client = await connectedToDatabase();
   const db = client.db();
-  const hashedPassword = hashPassword(password);
+  const hashedPassword = await hashPassword(password);
 
   const result = await db
     .collection('users')
