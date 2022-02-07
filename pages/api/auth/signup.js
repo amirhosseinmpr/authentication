282d1aebfc1fb5b483622aba51c6e1317a2e920a
amir hoseein <amiirhossein.mpr@gmail.com>
2022-02-07 15:58:37 +0330

    Adding A User Signup API Route

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,27 @@
+import { connectedToDatabase } from '../../../lib/db';
+import { hashPassword } from '../../../lib/auth';
+function Handler(req, res) {
+  const data = req.body;
+  const { email, password } = data;
+
+  if (
+    !email ||
+    !email.includes('@') ||
+    !password ||
+    !password.trim().length < 7
+  ) {
+    res.status(422).json({ message: 'Invalid input' });
+    return;
+  }
+
+  const client = await connectedToDatabase();
+  const db = client.db();
+  const hashedPassword = hashPassword(password);
+
+  const result = await db
+    .collection('users')
+    .insertOne({ email: email, password: hashedPassword });
+
+  res.status(201).json({ message: 'Created user ' });
+}
+export default Handler;
