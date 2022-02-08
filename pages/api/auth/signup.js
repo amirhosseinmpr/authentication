540746962e032a768653d219c004a56083c8a18c
amir hoseein <amiirhossein.mpr@gmail.com>
2022-02-09 00:24:32 +0330

    fixed fetch

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,5 +1,5 @@
 import { hashPassword } from '../../../lib/auth';
-import { connectToDatabase } from '../../../lib/db';
+import { connectedToDatabase } from '../../../lib/db';
 
 async function handler(req, res) {
   if (req.method !== 'POST') {
@@ -9,19 +9,11 @@ async function handler(req, res) {
   const data = req.body;
   const { email, password } = data;
 
-  const client = await connectToDatabase();
+  const client = await connectedToDatabase();
 
   const db = client.db();
 
-  const existingUser = await db.collection('users').findOne({ email: email });
-
-  if (existingUser) {
-    res.status(422).json({ message: 'User exists already!' });
-    client.close();
-    return;
-  }
-
-  const hashedPassword = await hashPassword(password);
+  const hashedPassword = hashPassword(password);
 
   const result = await db.collection('users').insertOne({
     email: email,
